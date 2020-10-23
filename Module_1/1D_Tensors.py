0e5e77244e9eaddbe0da6044f45ec3a64e3f333a
Charrybot <junehahwang@gmail.com>
2020-10-23 22:13:04 +0900

    commit lmao

--- Module_1/1D_Tensors.py
+++ Module_1/1D_Tensors.py
@@ -5,4 +5,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# Plot vecotrs, please keep the parameters in the same length
+# @param: Vectors = [{"vector": vector variable, "name": name of vector, "color": color of the vector on diagram}]
+
+def plotVec(vectors):
+    ax = plt.axes()
+
+    # For loop to draw the vectors
+    for vec in vectors:
+        ax.arrow(0, 0, *vec["vector"], head_width=0.05, color=vec["color"], head_length=0.1)
+        plt.text(*(vec["vector"] + 0.1), vec["name"])
+
+    plt.ylim(-2, 2)
+    plt.xlim(-2, 2)
+
+
+# Convert a integer list with length 5 to a tensor
+ints_to_tensor = torch.tensor([0, 1, 2, 3, 4])
+print("The dtype of tensor object after converting it to tensor: ", ints_to_tensor.dtype)
+print("The type of tensor object after converting it to tensor: ", ints_to_tensor.type())
+
+# Convert a float list with length 5 to a tensor
+floats_to_tensor = torch.tensor([0.0, 1.0, 2.0, 3.0, 4.0])
+print("The dtype of tensor object after converting it to tensor: ", floats_to_tensor.dtype)
+print("The type of tensor object after converting it to tensor: ", floats_to_tensor.type())
+
+
+list_floats = [0.0, 1.0, 2.0, 3.0, 4.0]
+
+floats_int_tensor = torch.tensor(list_floats, dtype=torch.int64)
+
+print("The dtype of tensor object is: ", floats_int_tensor.dtype)
+print("The type of tensor object is: ", floats_int_tensor.type())
+
 
